088cbf5fb7cf12472d5842d7151d32a5cb218724
Interlogicaldummy <47039638+Interlogicaldummy@users.noreply.github.com>
2019-03-19 18:19:04 +0000

    Add files via upload

--- js.js
+++ js.js
@@ -3,7 +3,6 @@ $(document).ready(
     $('.menu-toggle').click(function() {
       $('nav').toggleClass('active')
     })
-
     $('ul li').click(function() {
       $(this).siblings().removeClass('active');
       $(this).toggleClass('active');
@@ -11,10 +10,35 @@ $(document).ready(
   }
 )
 
-function toggleClass(el) {
-  if (el.className == "fa fa-bars") {
-    el.className = "fas fa-times";
-  } else {
-    el.className = "fa fa-bars";
+var $h = $(window);
+var height = $h.height();
+var prevScrollpos = window.pageYOffset,
+    $h = $(window),
+    height = $h.height();
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if ($(this).scrollTop() > height ) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-5vw";
+    }
+    prevScrollpos = currentScrollPos;
   }
 }
+
+$(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('header').addClass('nav-after');
+            $('header nav ul li ul').addClass('sub-aft');
+            $('header nav ul li ul a').addClass('sub-a-aft');
+            $('#ye-sub').addClass('ye-sub');
+      }
+
+      else {
+            $('header').removeClass('nav-after');
+            $('header nav ul li ul').removeClass('sub-aft');
+            $('header nav ul li ul a').removeClass('sub-a-aft');
+            $('#ye-sub').removeClass('ye-sub');
+      }
+})
